6d182b2a89122d4e9b78681346923b7fcabcf118
Miquel Soler Serra <miquel@imac-de-miquel-soler-serra.local>
2019-07-24 00:54:57 +0200

    detalle datos articulo

--- src/app/articulo/articulo.component.ts
+++ src/app/articulo/articulo.component.ts
@@ -3,6 +3,7 @@ import { DatosArticulo } from '../_entities/DatosArticulo';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { AlertService } from '../_services/alert.service';
+import { AlertType } from '../_entities/Alert';
 import { Tabla2Service} from '../tabla2.service';
 import { Element } from '../Element';
 
@@ -15,6 +16,7 @@ export class ArticuloComponent implements OnInit {
 
   idPosicion: number;
   posicion: Element;
+  datosArticulo: DatosArticulo;
   routingSubscription: any;
 
   constructor(private route: ActivatedRoute,
@@ -30,13 +32,29 @@ export class ArticuloComponent implements OnInit {
     this.routingSubscription = this.route.params.subscribe(params => {
       this.idPosicion = params["idPosicion"];
       console.log("this.service.currPedido="+this.service.currPedido );
-      console.log("this.service.currPosiciones="+JSON.stringify( this.service.currPosiciones) );
+      //console.log("this.service.currPosiciones="+JSON.stringify( this.service.currPosiciones) );
      
       this.posicion = this.service.currPosiciones.filter( row => row.id == this.idPosicion )[0];
       
-      console.log("idPosicion=" + this.idPosicion );
-      console.log("this.posicion = " + this.posicion );
+      console.log("codigo =" + this.posicion.codigo );
+      console.log("datos articulos ="+JSON.stringify(  this.service.datosArticulos ));
       
+      this.datosArticulo = this.service.datosArticulos.filter( row => row.codigo == this.posicion.codigo )[0];
+      if  (!(this.datosArticulo))
+      this.service.obtenerArticulo(this.posicion.codigo, "0051" ).subscribe(reply => {
+        switch (reply.codError) {
+          case 0:
+            //this.alert.sendAlert(reply.mensaje, AlertType.Success);
+            this.datosArticulo =  reply;
+            console.log("caja = "+this.datosArticulo.caja+ "manto = "+this.datosArticulo.manto );
+            break;
+          default:
+            this.alert.sendAlert(reply.mensaje, AlertType.Error);
+            break;
+        }
+        
+      });
+
     
     });
   }
